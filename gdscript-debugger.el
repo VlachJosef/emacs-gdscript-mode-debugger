d9bcef17607e3b5ad7e2031379a610a64b689103
Josef Vlach <vlach.josef@gmail.com>
2020-08-27 08:56:50 +0100

    Don't skip last packet. get-stack-dump command implemented

--- gdscript-debugger.el
+++ gdscript-debugger.el
@@ -32,12 +32,22 @@
     ;;(eval (message "IN A INTEGER %s" last))
     ))
 
+(defvar gdscript-debugger--float-spec
+  '((:float-data u32r) ;; How to read float?
+    ;;(eval (message "IN A FLOAT %s" last))
+    ))
+
 (defvar gdscript-debugger--string-spec
   '((:data-length u32r)
     (:string-data str (:data-length))
     (align 4)
     ;; (eval (message "IN A STRING %s %s" last bindat-idx))
     ))
+
+(defvar gdscript-debugger--dictionary-spec
+  '((:dictionary-length   u32r)
+    ;;(eval (message "DICTIONARY size: %s" last))
+    (:items repeat (:dictionary-length) (struct godot-data-bindat-spec))))
 
 (defvar gdscript-debugger--array-spec
   '((:array-length   u32r)
@@ -49,10 +59,41 @@
     ;;(eval (message "AFTER  IN A ARRAY %s" last))
     ))
 
+(defvar gdscript-debugger--pool-vector-2-array-spec
+  '((:array-length   u32r)
+    ;;(:string-data str (:data-length))
+    ;;(align 4)
+    ;;(logand (bindat-get-field struct :array-length) #x7FFFFFFF)
+    (eval (message "pool-vector-2-array-spec size: %s" last))
+    ;;(:items repeat (:array-length) (struct godot-data-bindat-spec))
+    ;;(eval (message "AFTER  IN A ARRAY %s" last))
+    ))
 
+(defvar gdscript-debugger--unknown-spec-5 '((eval (message "IN A SPEC 5"))))
+(defvar gdscript-debugger--unknown-spec-6 '((eval (message "IN A SPEC 6"))))
+(defvar gdscript-debugger--unknown-spec-7 '((eval (message "IN A SPEC 7"))))
+(defvar gdscript-debugger--unknown-spec-8 '((eval (message "IN A SPEC 8"))))
+(defvar gdscript-debugger--unknown-spec-9 '((eval (message "IN A SPEC 9"))))
+(defvar gdscript-debugger--unknown-spec-10 '((eval (message "IN A SPEC 10"))))
+(defvar gdscript-debugger--unknown-spec-11 '((eval (message "IN A SPEC 11"))))
+(defvar gdscript-debugger--unknown-spec-12 '((eval (message "IN A SPEC 12"))))
+(defvar gdscript-debugger--unknown-spec-13 '((eval (message "IN A SPEC 13"))))
+(defvar gdscript-debugger--unknown-spec-14 '((eval (message "IN A SPEC 14"))))
+(defvar gdscript-debugger--unknown-spec-15 '((eval (message "IN A SPEC 15"))))
+(defvar gdscript-debugger--unknown-spec-16 '((eval (message "IN A SPEC 16"))))
+(defvar gdscript-debugger--unknown-spec-17 '((eval (message "IN A SPEC 17"))))
+(defvar gdscript-debugger--unknown-spec-18 '((eval (message "IN A SPEC 18"))))
+(defvar gdscript-debugger--unknown-spec-20 '((eval (message "IN A SPEC 20"))))
+(defvar gdscript-debugger--unknown-spec-21 '((eval (message "IN A SPEC 21"))))
+(defvar gdscript-debugger--unknown-spec-22 '((eval (message "IN A SPEC 22"))))
+(defvar gdscript-debugger--unknown-spec-23 '((eval (message "IN A SPEC 23"))))
+;;(defvar gdscript-debugger--unknown-spec-24 '((eval (message "IN A SPEC 24"))))
+(defvar gdscript-debugger--unknown-spec-25 '((eval (message "IN A SPEC 25"))))
+(defvar gdscript-debugger--unknown-spec-26 '((eval (message "IN A SPEC 26"))))
+(defvar gdscript-debugger--unknown-spec-27 '((eval (message "IN A SPEC 27"))))
 
 (defvar gdscript-debugger--my-spec
-  '((eval (message "IN A SPEC %s" last))))
+  '((eval (message "IN A SPEC %s" (bindat-get-field struct :data-type)))))
 
 (defvar godot-data-bindat-spec
   '((:data-type     u32r)
@@ -60,8 +101,31 @@
     (union (:data-type)
            (1 (struct gdscript-debugger--boolean-spec))
            (2 (struct gdscript-debugger--integer-spec))
+           (3 (struct gdscript-debugger--float-spec))
            (4 (struct gdscript-debugger--string-spec))
+           (5 (struct gdscript-debugger--unknown-spec-5))
+           (6 (struct gdscript-debugger--unknown-spec-6))
+           (7 (struct gdscript-debugger--unknown-spec-7))
+           (8 (struct gdscript-debugger--unknown-spec-8))
+           (9 (struct gdscript-debugger--unknown-spec-9))
+           (10 (struct gdscript-debugger--unknown-spec-10))
+           (11 (struct gdscript-debugger--unknown-spec-11))
+           (12 (struct gdscript-debugger--unknown-spec-12))
+           (13 (struct gdscript-debugger--unknown-spec-13))
+           (14 (struct gdscript-debugger--unknown-spec-14))
+           (15 (struct gdscript-debugger--unknown-spec-15))
+           (16 (struct gdscript-debugger--unknown-spec-16))
+           (17 (struct gdscript-debugger--unknown-spec-17))
+           (18 (struct gdscript-debugger--dictionary-spec))
            (19 (struct gdscript-debugger--array-spec))
+           (20 (struct gdscript-debugger--unknown-spec-20))
+           (21 (struct gdscript-debugger--unknown-spec-21))
+           (22 (struct gdscript-debugger--unknown-spec-22))
+           (23 (struct gdscript-debugger--unknown-spec-23))
+           (24 (struct gdscript-debugger--pool-vector-2-array-spec))
+           (25 (struct gdscript-debugger--unknown-spec-25))
+           (26 (struct gdscript-debugger--unknown-spec-26))
+           (27 (struct gdscript-debugger--unknown-spec-27))
            (t (struct gdscript-debugger--my-spec)))))
 
 (defvar gdscript-debugger--previous-packet-data nil)
@@ -108,7 +172,7 @@
 ;;     ;;(data vec (data-length) (eval (if t str str)))
 ;;     (align 4)))
 
-(defun gdscript-debugger--next-packet (content offset)
+(defun gdscript-debugger--current-packet (content offset)
   (bindat-unpack gdscript-debugger--packet-length-bindat-spec content offset))
 
 (defun gdscript-debugger--process-packet (content offset)
@@ -117,6 +181,7 @@
 (defun gdscript-debugger--handle-server-reply (process content)
   "Gets invoked whenever the server sends data to the client."
   (message "(DATA received): %s" (length content))
+  (message "(Old DATA): %s" (length gdscript-debugger--previous-packet-data))
 
   ;;(message "(stringp content): %s" (stringp content))
   ;;(message "(type-of content): %s" (type-of content))
@@ -128,22 +193,29 @@
          (offset 0))
     (message "(content received): %s" (length content))
     (while (< offset content-length)
-      (let* ((packet-length-data (gdscript-debugger--next-packet content offset))
+      (let* ((packet-length-data (gdscript-debugger--current-packet content offset))
              (packet-length (bindat-get-field packet-length-data :packet-length))
              (next-packet-offset (+ 4 offset packet-length)))
+        ;;(message "packet-length-data: %s" packet-length-data)
+        (message "offset %s packet-length     : %s" offset packet-length)
         (if (< next-packet-offset content-length)
             (let ((packet-data (gdscript-debugger--process-packet content (+ 4 offset))))
-              (setq offset next-packet-offset)
-              (message "packet-length-data: %s" packet-length-data)
-              (message "packet-length     : %s" packet-length)
-              (message "offset            : %s" offset)
-              (message "packet-data       : %s" packet-data))
+              (message "packet-data %s - %s       : %s" (+ 4 offset)  next-packet-offset packet-data)
+              (setq offset next-packet-offset))
           (progn
             (setq gdscript-debugger--previous-packet-data (substring content offset content-length))
-            (setq offset next-packet-offset)
+
             (message "UPS, we need more data!!!!!!!!!!!!!!!!!!!!!!!!!!! %s %s" next-packet-offset content-length)
+            (cond
+             ((eq next-packet-offset content-length)
+              (message "But since %s %s are equals we need to process last packet" next-packet-offset content-length)
+              (message "Last packet %s - %s" (+ 4 offset) next-packet-offset)
+              (let ((packet-data (gdscript-debugger--process-packet content (+ 4 offset))))
+                (message "packet-data %s - %s     : %s" (+ 4 offset)  next-packet-offset packet-data))
+              ))
+            (setq offset next-packet-offset) ;; to break out of loop
             )))))
-  (message "(DATA proces sed): %s" (length content)))
+  (message "(DATA processed): %s" (length content)))
 
 ;;(defvar current-process nil)
 
@@ -166,6 +238,10 @@
    ((eq (process-status process) 'closed)
     (message "EHHHH ???"))))
 
+(defun gdscript-debugger-get-stack-dump()
+  (let ((server-process (get-process (car server-clients))))
+    (message "%s %s" server-process (process-status server-process))
+    (process-send-string server-process (gdscript-debugger--command "get_stack_dump"))))
 
 (defun gdscript-debugger-continue()
   (let ((server-process (get-process (car server-clients))))
@@ -272,14 +348,24 @@
 
 (defun gdscript-debugger--command (command)
   (message "(gdscript-debugger--packet-definition (length command)): %s" (gdscript-debugger--packet-definition (length command)))
-  (bindat-pack
-   (gdscript-debugger--packet-definition (length command))
-   `((:packet-length . 24)
-     (:array-type . ,variant-array)
-     (:elements-count . 1)
-     (:type . ,variant-string)
-     (:string-length . ,(length command))
-     (:string-data . ,command))))
+  (let* ((command-alength (align-length command))
+         (packet-length (+ (* 4 4) command-alength)))
+    (message "packet-length: %s" packet-length)
+    (message "command-alength: %s" command-alength)
+    (bindat-pack
+     (gdscript-debugger--packet-definition (length command))
+     `((:packet-length . ,packet-length)
+       (:array-type . ,variant-array)
+       (:elements-count . 1)
+       (:type . ,variant-string)
+       (:string-length . ,(length command))
+       (:string-data . ,command)))))
+
+(defun align-length (string)
+  (let ((len (length string)))
+    (while (/= (% len 4) 0)
+      (setq len (1+ len)))
+    len))
 
 ;; (let ((s (gdscript-debugger--command)))
 ;;        (list (multibyte-string-p s)
